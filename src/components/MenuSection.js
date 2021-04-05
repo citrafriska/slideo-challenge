b2d95ae1feb6ed5d322fbd1eeb40d43a2f95a0f5
citrafriska <cfriskanita@gmail.com>
2021-04-05 23:16:11 +0700

    initial commit

--- src/components/MenuSection.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function MenuSection() {
-  return <div></div>;
-}
-
-export default MenuSection;
--- src/components/PreviewSection.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function PreviewSection() {
-  return <div></div>;
-}
-
-export default PreviewSection;
